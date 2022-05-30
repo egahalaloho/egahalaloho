55511b91f51961ca3e7dbc083eec588348637a7d
ega sihaloho <egasihaloho123@gmail.com>
2022-05-30 14:52:15 +0700

    apus lagi

--- js/myJs.js
+++
@@ -1,7 +0,0 @@
-const myProject = document.querySelectorAll(".myproject");
-myProject.forEach((project, i) => {
-  project.dataset.aos = "flip-down";
-  if (i !== 0) {
-    project.dataset.aosDelay = i * 100;
-  }
-});
